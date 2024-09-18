bc912a2c1ce7db6267bf8bdfe2c4f0f88d829f13
EC2 Default User <ec2-user@ip-10-0-10-67.eu-west-2.compute.internal>
2024-09-18 16:04:54 +0000

    app info server.js

--- server.js
+++ server.js
@@ -28,7 +28,10 @@ mongoose.connection.once("open", function () {
   console.log("Successfully connected to the database");
 });
 
-app.use("/api/todos", todoRoutes);
+app.get("/api/todos", (req, res) => {
+    res.send("Hello");
+});
+//app.use("/api/todos", todoRoutes);
 
 // listen for requests
 const PORT = process.env.PORT || 3000;
