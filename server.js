b935ad48bad548d689e9ec91331539dd1285b76e
EC2 Default User <ec2-user@ip-10-0-10-67.eu-west-2.compute.internal>
2024-09-18 15:05:56 +0000

    todo-routes

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const config = require("./config/default.config.js");
-const todoRoutes = require("./src/routes/todo.routes.js");
+//const todoRoutes = require("./src/routes/todo.routes.js");
 
 // create express app
 const app = express();
@@ -28,14 +28,21 @@ mongoose.connection.once("open", function () {
   console.log("Successfully connected to the database");
 });
 
-// Root route
-//app.get('/', (req, res) => {
-    //res.send('Hello, world!');
-//});
+'use strict';
+module.exports = function(app){
+	var todofunctions = require('../controllers/todo.controller');
 
-// Todo Routes
-app.route('/')
-app.use("/api/todos", todoRoutes);
+// todo routes
+app.route('/tasks')      //task [GET (all thetasks),POST]
+  .get(todofunctions.listTasks)
+  .post(todofunctions.createTask);
+
+app.route('/tasks/:taskId')  //a task [GET(single task),PUT,DELETE]
+  .put(todofunctions.updatetask)
+  .get(todofunctions.readTask)
+  .delete(todofunctions.deleteTask);
+};
+//app.use("/api/todos", todoRoutes);
 
 // listen for requests
 const PORT = process.env.PORT || 3000;
